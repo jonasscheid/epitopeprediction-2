ada9e48d33a636951245fb433ba63ce8c69d3aeb
jonasscheid <jonas.scheid@uni-tuebingen.de>
2023-09-26 15:47:21 +0000

    adjust netmhcpan pred

--- bin/netmhcpan.py
+++ bin/netmhcpan.py
@@ -52,7 +52,7 @@ def main():
                 logger.warning(f'{peptide} does not have the right length. Skipping..')
 
     # Check if input alleles are supported by netmhcpan
-    input_alleles = [allele.replace('*', '').replace(':','') for allele in args.alleles.split(';')]
+    input_alleles = [allele.replace('*', '') for allele in args.alleles.split(';')]
     # For this we need to catch the stdout of netmhcpan
     sp.run(['netmhcpan/netMHCpan', '-listMHC'], stdout=open('supported_alleles.txt', 'w'))
 
